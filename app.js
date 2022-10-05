cc31da3c9036be12d67a66fefb2c605c2b9fa97e
bdraihan71 <bdraihan71@gmail.com>
2022-10-05 15:24:46 +0600

    Add model

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello Raihan'
+    })
+})
+
+
+const PORT = process.env.PORT
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ models/Comment.js
@@ -0,0 +1,50 @@
+//post, user, body, replies
+
+const { Schema, model } = require('mongoose')
+const User = require('./User')
+const Post = require('./Post')
+
+const commnetSchema = new Schema({
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: Post,
+        required: true
+    },
+
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+
+    body: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    replies: [
+        {
+            body: {
+                type: String,
+                required: true
+            },
+
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: User,
+                required: true
+            },
+
+            createAt: {
+                type: Date,
+                default: new Date()
+            }
+        }
+    ]
+}, {
+    timestamps: true
+})
+
+const Comment = model('Comment', commnetSchema)
+module.exports = Comment
